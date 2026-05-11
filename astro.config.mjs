49e2375c3f179d4134b74f2449cb4486ba942764
mariorey <marioreymario@gmail.com>
2026-05-11 18:40:02 +0200

    checkpoint 11 may - first deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://mariorey.github.io',
+  base: '/hanyu-ly',
   integrations: [tailwind()],
 });
